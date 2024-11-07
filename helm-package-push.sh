f6b8ee4b8664d84aed108ff1842b002ac87eecbb
Julian Derks <derks_julian@hotmail.com>
2024-11-07 13:15:05 +0100

    update

--- helm-package-push.sh
+++ helm-package-push.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# check env vars!
-# https://skaffold.dev/docs/lifecycle-hooks/
+# Check if Skaffold is configured to push images
+if [ "$SKAFFOLD_PUSH_IMAGE" != "true" ]; then
+  echo "Skipping Helm chart packaging and push since push is disabled."
+  exit 0
+fi
 
-echo SKAFFOLD_IMAGE_TAG=$SKAFFOLD_IMAGE_TAG
+# # Debugging information
+# echo "SKAFFOLD_IMAGE_TAG=$SKAFFOLD_IMAGE_TAG"
+# echo "SKAFFOLD_PUSH_IMAGE=$SKAFFOLD_PUSH_IMAGE"
 
-# Package the Helm chart
-helm package chart --destination ./ --version $SKAFFOLD_IMAGE_TAG --app-version $SKAFFOLD_IMAGE_TAG
+# Package the Helm chart with version and app-version based on image tag
+helm package chart --destination ./ --version "$SKAFFOLD_IMAGE_TAG" --app-version "$SKAFFOLD_IMAGE_TAG"
 
 # Push the packaged chart to Artifact Registry using OCI support
-helm push *.tgz oci://europe-west4-docker.pkg.dev/xx
+helm push *.tgz oci://europe-west4-docker.pkg.dev/shared-9456/sng-helm
 
 # Clean up the packaged .tgz file to keep things tidy
 rm *.tgz
